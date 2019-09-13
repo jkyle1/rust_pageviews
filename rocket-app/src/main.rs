4b78685453400ca9f42ae7fc53e0f9cc6bc9ff15
kyle.smallwood@gmail.com <kyle.smallwood@gmail.com>
2019-09-13 13:52:05 +0100

    initial commit cors/model/routes; added migrations up/down sql and generated schema; added crate dependencies, models and code to main

--- rocket-app/src/main.rs
+++ rocket-app/src/main.rs
@@ -1,3 +1,28 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+extern crate chrono;
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+
+pub mod cors;
+pub mod models;
+pub mod routes;
+pub mod schema;
+
+#[database("rocket_app")]
+pub struct DbConn(diesel::MysqlConnection);
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![/* TODO */]
+            .attach(DbConn::fairing())
+            .launch();
 }
